d5f3bbf0e156cb6366045328891f4b4076e6ec09
Arnolgs <50081183+Arnolgs@users.noreply.github.com>
2020-08-07 00:31:14 -0700

    Create colorfilter.py
    Defines and tests a function for color filtering.  Uses this
    function to define functions for lightening and darkening images.

---
+++ colorfilter.py
@@ -0,0 +1,45 @@
+"""
+File: colorfilter.py
+Project 7.9
+
+
+"""
+
+from images import Image
+
+def colorFilter(image, rgbTriple):
+    def baseValue(value, offset):
+        if offset == 0:
+            return value
+        elif offset < 0:
+            return max(value + offset, 0)
+        else:
+            return min(value + offset, 255)
+
+    (r, g, b) = rgbTriple
+    for y in range(image.getHeight()):
+        for x in range(image.getWidth()):
+            (red, green, blue) = image.getPixel(x, y)
+            (red, green, blue) = (baseValue(red, r), baseValue(green, g),baseValue(blue, b))
+            image.setPixel(x, y, (red, green, blue))  
+
+def lighten(image, amount):
+    colorFilter(image, (amount, amount, amount))
+        
+
+def darken(image, amount):
+    colorFilter(image, (-amount, -amount, -amount))   
+            
+
+def main():
+    filename = input("Enter the image file name: ")
+    image = Image(filename)
+    lighten(image, 20)
+    darken(image2, 64)
+    colorFilter(image3, (255, 0, 0))
+    image.draw()
+
+if __name__ == "__main__":
+   main()
+
+
